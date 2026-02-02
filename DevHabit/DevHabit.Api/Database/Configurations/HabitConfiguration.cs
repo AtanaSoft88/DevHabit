4b514d029ccfadb70b3bd47717ae3799fb5192d6
Atanas Atanasov <djx700@abv.bg>
2026-02-02 17:14:18 +0200

    Postgres SQL configuration in docker

--- DevHabit/DevHabit.Api/Database/Configurations/HabitConfiguration.cs
+++ DevHabit/DevHabit.Api/Database/Configurations/HabitConfiguration.cs
@@ -1,5 +1,28 @@
-﻿namespace DevHabit.Api.Database.Configurations;
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class HabitConfiguration
+namespace DevHabit.Api.Database.Configurations;
+
+public sealed class HabitConfiguration : IEntityTypeConfiguration<Habit>
 {
+    public void Configure(EntityTypeBuilder<Habit> builder)
+    {
+        builder.HasKey(h => h.Id);
+
+        builder.Property(h => h.Id)            
+                    .HasMaxLength(500);
+        builder.Property(h => h.Name)                    
+                    .HasMaxLength(100);
+        builder.Property(h => h.Description)
+                    .HasMaxLength(500);
+        builder.OwnsOne(f => f.Frequency);
+
+        builder.OwnsOne(t => t.Target, targetBuilder => 
+        {
+            targetBuilder.Property(t => t.Unit).HasMaxLength(50);
+        });
+
+        builder.OwnsOne(m => m.Milestone);
+    }
 }
