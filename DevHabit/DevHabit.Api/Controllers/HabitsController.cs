d8deebe74f4fe30816b1c8b86d9273bd2d2bbe7a
Atanas Atanasov <djx700@abv.bg>
2026-02-03 21:47:06 +0200

    Setup Habit controller for getting resources

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -1,4 +1,5 @@
 ﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,9 +11,84 @@ namespace DevHabit.Api.Controllers;
 public sealed class HabitsController(ApplicationDbContext dbContext) : ControllerBase
 {
     [HttpGet()]
-    public async Task<IActionResult> GetHabits()
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabits()
     {
-        List<Habit> habits = await dbContext.Habits.ToListAsync();
-        return Ok(habits);
+        List<HabitDto> habits = await dbContext.Habits
+            .Select(habit => new HabitDto
+            {
+                Id = habit.Id,
+                Name = habit.Name,
+                Description = habit.Description,
+                Type = habit.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = habit.Frequency.Type,
+                    TimesPerPeriod = habit.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Value = habit.Target.Value,
+                    Unit = habit.Target.Unit
+                },
+                Status = habit.Status,
+                IsArchived = habit.IsArchived,
+                EndDate = habit.EndDate,
+                Milestone = habit.Milestone == null ? null : new MilestoneDto
+                {
+                    Target = habit.Milestone.Target,
+                    Current = habit.Milestone.Current
+                },
+                CreatedAtUtc = habit.CreatedAtUtc,
+                UpdatedAtUtc = habit.UpdatedAtUtc,
+                LastCompletedAtUtc = habit.LastCompletedAtUtc
+            })
+            .ToListAsync();
+
+        var habitsCollectionDto = new HabitsCollectionDto
+        {
+            Data = habits
+        };
+
+        return Ok(habitsCollectionDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<HabitDto>> GetHabitById(string id)
+    {
+        HabitDto? habit = await dbContext.Habits
+            .Where(habit => habit.Id == id)
+            .Select(habit => new HabitDto
+            {
+                Id = habit.Id,
+                Name = habit.Name,
+                Description = habit.Description,
+                Type = habit.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = habit.Frequency.Type,
+                    TimesPerPeriod = habit.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Value = habit.Target.Value,
+                    Unit = habit.Target.Unit
+                },
+                Status = habit.Status,
+                IsArchived = habit.IsArchived,
+                EndDate = habit.EndDate,
+                Milestone = habit.Milestone == null ? null : new MilestoneDto
+                {
+                    Target = habit.Milestone.Target,
+                    Current = habit.Milestone.Current
+                },
+                CreatedAtUtc = habit.CreatedAtUtc,
+                UpdatedAtUtc = habit.UpdatedAtUtc,
+                LastCompletedAtUtc = habit.LastCompletedAtUtc
+            })
+            .FirstOrDefaultAsync();
+
+         return habit is null ?
+                      NotFound() :
+                      Ok(habit);
     }
 }
