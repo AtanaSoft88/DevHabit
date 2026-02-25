720d344078fdf96e17a9c1cac5cbb2960f827dbd
Atanas Atanasov <djx700@abv.bg>
2026-02-25 23:35:18 +0200

    Refactoring Program file using extensions

---
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -0,0 +1,111 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
+using DevHabit.Api.Middleware;
+using DevHabit.Api.Services;
+using DevHabit.Api.Services.Sorting;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json.Serialization;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace DevHabit.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddControllers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers(options =>
+        {
+            options.ReturnHttpNotAcceptable = true; //Added to return 406 when the requested format is not supported    
+
+        })
+        // Configure Newtonsoft.Json to use camel case property names in JSON responses
+        .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver())
+        .AddXmlSerializerFormatters(); //Added XmlSerializerFormatters() to support XML format
+
+        builder.Services.AddOpenApi();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddErrorHandling(this WebApplicationBuilder builder)
+    {
+        // Add ProblemDetails support and customize it to include the requestId in the extensions
+        builder.Services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                // Add the requestId to the ProblemDetails extensions for better traceability
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+            };
+        });
+
+        // Register the validation exception handler middleware to handle FluentValidation exceptions
+        builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+
+        // Register the global exception handler middleware
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+
+         opt
+            .UseNpgsql(
+               builder.Configuration.GetConnectionString("PostgresDatabase"),
+               npgsql => npgsql.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+            .UseSnakeCaseNamingConvention());
+
+        return builder; 
+    }
+
+    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder) 
+    {
+        // Configure OpenTelemetry for tracing and metrics collection, including instrumentation for HTTP client, ASP.NET Core, and Npgsql, and set up the OTLP exporter to send telemetry data to a compatible backend
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+            .WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddNpgsql())
+            .WithMetrics(metrics => metrics
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation())
+            .UseOtlpExporter();
+
+        // Configure OpenTelemetry logging to include scopes and formatted messages, which can provide more context and readability in the logs
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeScopes = true;
+            options.IncludeFormattedMessage = true;
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder) 
+    {
+        // Add FluentValidation support from the assembly containing the Program class
+        builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+        // Register the sort mapping provider as a transient service to provide sort mappings for different DTO and entity combinations
+        builder.Services.AddTransient<SortMappingProvider>();
+        // Register the sort mapping definition for HabitDto and Habit to enable sorting based on the defined mappings
+        builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ => HabitMappings.SortMapping);
+        // Register the data shaping service as a transient service to enable shaping of data based on specified fields in the API responses
+        builder.Services.AddTransient<DataShapingService>();
+
+        return builder;
+
+    }
+}
--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,86 +1,13 @@
-using DevHabit.Api.Database;
-using DevHabit.Api.DTOs.Habits;
-using DevHabit.Api.Entities;
+using DevHabit.Api;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middleware;
-using DevHabit.Api.Services;
-using DevHabit.Api.Services.Sorting;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json.Serialization;
-using Npgsql;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true; //Added to return 406 when the requested format is not supported    
-
-})
-// Configure Newtonsoft.Json to use camel case property names in JSON responses, which is a common convention in JSON APIs and can improve readability for clients consuming the API.
-.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver())
-.AddXmlSerializerFormatters(); //Added XmlSerializerFormatters() to support XML format
-
-// Add FluentValidation support from the assembly containing the Program class
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-// Add ProblemDetails support and customize it to include the requestId in the extensions
-builder.Services.AddProblemDetails(options => 
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        // Add the requestId to the ProblemDetails extensions for better traceability
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-
-// Register the validation exception handler middleware to handle FluentValidation exceptions
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-
-// Register the global exception handler middleware
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-
-    opt
-       .UseNpgsql(
-         builder.Configuration.GetConnectionString("PostgresDatabase"),
-         npgsql => npgsql.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-       .UseSnakeCaseNamingConvention());
-
-// Configure OpenTelemetry for tracing and metrics collection, including instrumentation for HTTP client, ASP.NET Core, and Npgsql, and set up the OTLP exporter to send telemetry data to a compatible backend
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-    .WithTracing(tracing => tracing
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddNpgsql())
-    .WithMetrics(metrics => metrics
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddRuntimeInstrumentation())
-    .UseOtlpExporter();
-
-// Configure OpenTelemetry logging to include scopes and formatted messages, which can provide more context and readability in the logs
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeScopes = true;
-    options.IncludeFormattedMessage = true;
-});
-
-// Register the sort mapping provider as a transient service to provide sort mappings for different DTO and entity combinations
-builder.Services.AddTransient<SortMappingProvider>();
-// Register the sort mapping definition for HabitDto and Habit to enable sorting based on the defined mappings
-builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ => HabitMappings.SortMapping);
-// Register the data shaping service as a transient service to enable shaping of data based on specified fields in the API responses
-builder.Services.AddTransient<DataShapingService>();
+builder.AddControllers()
+       .AddErrorHandling()
+       .AddDatabase()
+       .AddObservability()
+       .AddApplicationServices();
 
 WebApplication app = builder.Build();
 
